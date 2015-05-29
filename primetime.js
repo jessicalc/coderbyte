91a8ff685b1523c60dda8e2f0236122367f4c093
jessicalc <jessica@crayoncollective.com>
2015-05-30 03:12:59 +0800

    refactor #2
    am I even using return properly?

--- primetime.js
+++ primetime.js
@@ -1,17 +1,17 @@
 function PrimeTime(num) { 
+  var isPrime = false;
   var divisibleByNumber = 0;
-  // divide it by all the numbers before it. if divisible (i.e. remainder is zero),
-  // then add it to the divisibleByNumber counter.
   for (i = 2; i < num; i++) {
     if (num % i === 0) {
       divisibleByNumber++;
+      if (divisibleByNumber > 0) {
+        isPrime = false;
+        return num + " is not a prime number";
+      }
+    } else {
+      return num + " is a prime number.";
     }
-  }  
-  if (divisibleByNumber > 0) {
-    return num + " is not a prime number.";
-  } else {
-    return num + " is a prime number.";
   }
 }
-console.log(PrimeTime(809));
+console.log(PrimeTime(22));
 console.log(PrimeTime(3));
